bd27008589e3f56444301ed1e0e1ec00f3cc5e3f
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-04-24 21:22:53 +0300

    "add exceptionNotEnoughMoneyHandler method"

---
+++ src/main/java/exceptions/ExceptionControllerAdvice.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+    @ExceptionHandler(NotEnoughMoneyException.class)
+    public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Недостаточно денег для оплаты.");
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+
+}
